2fe1f6a1617f8216d46caf0551cbb2a8253119c7
Fabricio Henrique <fabriciohod@hotmail.com>
2020-03-27 09:42:22 -0300

    _

--- Questão10/Program.cs
+++ Questão10/Program.cs
@@ -4,9 +4,18 @@ namespace Questão10
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write ("Digite a quantidade de dias: ");
+            int dias = int.Parse (Console.ReadLine ());
+            System.Console.WriteLine (AnosMesesDias (dias));
+        }
+        static string AnosMesesDias (int dias)
+        {
+            var _anos = dias / 365;
+            var _meses = _anos / 30;
+            var _dias = _meses / 30;
+            return $"{_anos} ano(s), {_meses} mes(es), {_dias} dia(s)";
         }
     }
 }
