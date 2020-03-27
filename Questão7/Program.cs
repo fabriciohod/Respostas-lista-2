9b10847917e579301660d22550ed2d2803200620
Fabricio Henrique <fabriciohod@hotmail.com>
2020-03-27 08:28:24 -0300

    _

--- Questão7/Program.cs
+++ Questão7/Program.cs
@@ -4,9 +4,18 @@ namespace Questão7
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write ("Digite sua iade: ");
+            int idade = int.Parse (Console.ReadLine ());
+            Console.WriteLine (ClassificarAtleta (idade));
+        }
+        static string ClassificarAtleta (int idade)
+        {
+            if (idade <= 5 && idade >= 10) return "Voce podera jogar na categoria Infantil";
+            else if (idade >= 11 && idade <= 15) return "Voce podera jogar na categoria Juvenil";
+            else if (idade >= 16 && idade <= 20) return "Voce podera jogar na categoria Junior";
+            else return "Voce podera jogar na categoria Profissional";
         }
     }
 }
