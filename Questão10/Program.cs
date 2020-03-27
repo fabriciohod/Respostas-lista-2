2e96febb73cb69ac9a5d7eb922156bdb80f82c30
Fabricio Henrique <fabriciohod@hotmail.com>
2020-03-27 09:33:13 -0300

    _

---
+++ Questão10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Questão10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
